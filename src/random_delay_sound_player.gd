75b5899f426bf4d09d1488e175f20e697d2c0da9
Moritz <moprimu@hotmail.de>
2026-06-20 23:17:29 +0200

    Various changes: - Added stages - Added multiple sprites - Made moses - Added sound effects

---
+++ src/random_delay_sound_player.gd
@@ -0,0 +1,15 @@
+extends AudioStreamPlayer2D
+
+@export var _min_wait_time := 20.0
+@export var _max_wait_time := 40.0
+
+func _ready() -> void:
+	_wait_and_play_athmo_sound()
+
+func _wait_and_play_athmo_sound() -> void:
+	var wait_time := randf_range(_min_wait_time, _max_wait_time)
+	print("Waiting %s for next playback" % wait_time)
+	await get_tree().create_timer(wait_time).timeout
+	play()
+	await finished
+	_wait_and_play_athmo_sound()
